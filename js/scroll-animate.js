b29f72b67f172eef11914a7ee0da5506114f6eb2
Ptaha04 <112693513+Ptaha04@users.noreply.github.com>
2025-06-10 01:03:15 +0200

    Update scroll-animate.js

--- js/scroll-animate.js
+++ js/scroll-animate.js
@@ -9,6 +9,10 @@ const observer = new IntersectionObserver(entries => {
     threshold: 0.2
 });
 
-document.querySelectorAll('.section').forEach(section => {
+/*document.querySelectorAll('.section').forEach(section => {
     observer.observe(section);
+}); */
+
+document.querySelectorAll('.section').forEach(section => {
+  section.classList.add('visible');
 });
